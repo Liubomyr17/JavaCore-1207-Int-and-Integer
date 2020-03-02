53230f739a5a3189469b4c555ff71a672c1a88f0
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-03-02 21:22:57 +0200

    Add files via upload

---
+++ JavaCore 1207 Int and Integer/src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+/*
+
+1207 Int and Integer
+Write two methods: print (int) and print (Integer).
+Write such code in the main method so that both of them are called.
+
+Requirements:
+1. The Solution class must contain the static main () method.
+2. The Solution class must contain a static print () method with an int parameter.
+3. The Solution class must contain a static print () method with a parameter of type Integer.
+4. The main () method should call the print () method with an int parameter.
+5. The main () method should call the print () method with a parameter of type Integer.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        Main.print(1);
+        Main.print(new Integer(1));
+    }
+    public static void print(int s) {
+
+    }
+
+    public static void print(Integer s) {
+
+    }
+}
+
